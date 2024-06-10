f2aacb119fe0817d20c25313935e37170ce4c4f9
Aidar <abektemirov@innovationdigital.kg>
2024-06-10 14:55:45 +0600

    GF ready

--- src/Components/MainCanvas.svelte
+++ src/Components/MainCanvas.svelte
@@ -1,43 +1,120 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import * as PIXI from "pixi.js";
-
+  import {Assets} from 'pixi.js'
+  import {config} from '../Data/config'
+  import { writable, type Writable } from "svelte/store";
+  const toScale =(sprite:PIXI.Sprite,app:PIXI.Application):number =>{
+    const scale = Math.max(app.screen.width / sprite.width, app.screen.height / sprite.height)*0.50;
+    console.log('scale,sprite :>> ', scale,sprite);
+    return scale
+  }
   let app: PIXI.Application;
   let view: HTMLCanvasElement;
-  let texture: PIXI.Texture;
-  let texture2:PIXI.Texture
+  const floor:Writable<string> = writable('GF')
+   
   onMount(async() => {
       view.width = window.innerWidth;
       view.height = window.innerHeight;
       app = new PIXI.Application();
-  
-      await app.init({ view, backgroundColor: 0x1099bb, width: window.innerWidth, height: window.innerHeight, autoDensity: true, resolution: devicePixelRatio, }); 
-      texture = await PIXI.Assets.load('/public/Plot.png');
-      texture2 = await PIXI.Assets.load('/public/Unit.png');
-      const sprite = new PIXI.Sprite(texture);
-      const sprite2 = new PIXI.Sprite(texture2)
-      const sprite3= new PIXI.Sprite(texture2)
-      const scale = Math.max(app.screen.width / sprite.width, app.screen.height / sprite.height)*0.50;
+
+      await app.init({ view,backgroundColor:'#55432F',  width: window.innerWidth, height: window.innerHeight, autoDensity: true, resolution: devicePixelRatio, }); 
+      for(let option in config.GF){
+        if(option ==='plot'){
+          const texture = await PIXI.Assets.load(config.GF[option].src)
+          texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+          const sprite = new PIXI.Sprite(texture);
+
+       if(config.GF[option].positionRule==='enabled'){
+         sprite.x =config.GF[option]?.x
+         sprite.y = config.GF[option]?.y
+        }else if (config.GF[option].positionRule==='disabled'){
+          sprite.x = (app.screen.width - sprite.width) / 2
+          sprite.y = (app.screen.width - sprite.width) / 2
+        }
+          if(config.GF[option].sizeRule==='enabled'){
+              sprite.width = config.GF[option].width
+              sprite.height = config.GF[option].height
+            }else{
+              sprite.scale.set(toScale(sprite,app));
+            }
+          app.stage.addChild(sprite);
+        }else if (option ==='unit'){
+          const texture = await PIXI.Assets.load(config.GF[option].src)
+          texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+          const sprite = new PIXI.Sprite(texture);
+        
+         
       
+          sprite.scale.set(toScale(sprite,app));
+       if(config.GF[option].positionRule==='enabled'){
+         sprite.x =   config.GF[option]?.x
+         sprite.y = config.GF[option]?.y
+        }else if (config.GF[option].positionRule==='disabled'){
+          sprite.x = (app.screen.width - sprite.width) / 2 
+          sprite.y = (app.screen.width - sprite.width) / 2
+        }
+          if(config.GF[option].sizeRule==='enabled'){
+              sprite.width = config.GF[option].width
+              sprite.height = config.GF[option].height
+            }else if (config.GF[option].sizeRule==='disabled'){
+              sprite.scale.set(toScale(sprite,app));
+            }
+          app.stage.addChild(sprite);
+        }else if (option ==='options'){
+          const texture:PIXI.Texture = await PIXI.Assets.load(config.GF[option].element.src)
+          const sprite = new PIXI.Sprite(texture);
+          texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+         if(config.GF[option].element.positionRule==='enabled'){
+
+           sprite.x = config.GF[option].element.x ===undefined ?sprite.x = (app.screen.width - sprite.width) / 2:  config.GF[option].element.x
+          
+          
+          sprite.y = config.GF[option].element.x ===undefined ?sprite.y = (app.screen.width - sprite.width) / 2: config.GF[option].element.y
+          }else if(config.GF[option].element.positionRule==='disabled'){
+            sprite.x = (app.screen.width - sprite.width) / 2 
+            sprite.y = (app.screen.width - sprite.width) / 2
+          }
+       
+       
+          if(config.GF[option].element.sizeRule==='enabled'){
+              sprite.width = config.GF[option].element.width
+              sprite.height = config.GF[option].element.height
+            }else if (config.GF[option].element.sizeRule==='disabled'){
+              sprite.scale.set(toScale(sprite,app));
+            }
+      
+          //?      | config.GF[option].element.x === false ? sprite.x = (app.screen.width - sprite.width) / 2; : config.GF[option].element.x
+          //?      |config.GF[option].element.y === false ?sprite.y = (app.screen.height - sprite.height) / 2;   : config.GF[option].element.y
     
-      sprite.scale.set(scale);
-      sprite2.scale.set(scale)
-      sprite3.scale.set(scale)
-      sprite.x = (app.screen.width - sprite.width) / 2;
-      sprite.y = (app.screen.height - sprite.height) / 2;
-      sprite2.x = (app.screen.width - sprite2.width) / 2;
-      sprite2.y = (app.screen.height - sprite2.height) / 2;
-      sprite3.x = (app.screen.width - sprite3.width) / 2;
-      sprite3.y = (app.screen.height - sprite3.height) / 2;
-      
-      app.stage.addChild(sprite);
-      app.stage.addChild(sprite2)
-      app.stage.addChild(sprite3)
-      console.log('app :>> ', app.stage.children);
+          app.stage.addChild(sprite);
+        }
+      }
+    
+    
+     
+     
+   
+     
   });
+  $:if($floor ==='1F'){
+    addBunny(app)
+   
+  }
+  const addBunny = async(app:PIXI.Application) =>{
+    const texture =  await Assets.load('https://pixijs.com/assets/bunny.png');   
+    const sprite = new PIXI.Sprite(texture)
+
+   return app.stage.addChild(sprite)
+  }
+  $:console.log('$floor :>> ', $floor);
 </script>
 
 <canvas id="application" bind:this={view}></canvas>
+
+<div class="buttons">
+<button on:click={()=>$floor='1F'}>GF trigger</button>
+</div>
 
 <style lang="scss">
   #application {
---
+++ src/types/types.ts
@@ -0,0 +1,70 @@
+interface IConfig {
+  GF: {
+    plot: {
+      x: number;
+      y: number;
+      positionRule: string;
+      width: number;
+      height: number;
+      sizeRule: string;
+      src: string;
+      opacity: number;
+    };
+    unit: {
+      x: number;
+      y: number;
+      positionRule: string;
+      width: number;
+      height: number;
+      sizeRule: string;
+      src: string;
+      opacity: number;
+    };
+    options: {
+      element: {
+        x: 400;
+        y: 5;
+        positionRule: string;
+        width: number;
+        height: number;
+        sizeRule: string;
+        src: string;
+        opacity: number;
+      };
+    };
+  };
+  "1F": {
+    plot: {
+      x: number;
+      y: number;
+      positionRule: string;
+      width: number;
+      height: number;
+      sizeRule: string;
+      src: string;
+      opacity: number;
+    };
+    unit: {
+      x: number;
+      y: number;
+      positionRule: string;
+      width: number;
+      height: number;
+      sizeRule: string;
+      src: string;
+      opacity: number;
+    };
+    options: {
+      element: {
+        x: 400;
+        y: 5;
+        positionRule: string;
+        width: number;
+        height: number;
+        sizeRule: string;
+        src: string;
+        opacity: number;
+      };
+    };
+  };
+}
